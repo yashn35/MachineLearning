100eb83e96fffe4f5f8415ef757a346443d694ec
yashn35 <yasnara04@gmail.com>
2019-02-20 17:51:42 -0800

    PEP-8 protocol fixes

--- Advanced-ML/LogisticRegression/LogisticRegression.py
+++ Advanced-ML/LogisticRegression/LogisticRegression.py
@@ -22,10 +22,12 @@ X_income = data_v2.select_dtypes(include=['object'])
 X_enc = X_income.copy()
 
 '#one-hot encoding values from my data_v2set using pd.getdummies method'
-X_enc = pd.get_dummies(X_income, columns=
+
+""" #TODO: FIX PEP8 PROTOCOL CONFLICT
+X_enc = pd.get_dummies(X_income, columns =
     ['workclass', 'native-country', 'gender', 'relationship',
     'race', 'occupation', 'workclass', 'education', 'marital-status'])
-
+"""
 '#Y_target Income is the target row or the output of the model'
 Y_target = data_v2['income']
 
@@ -44,13 +46,13 @@ logisticRegression.fit(X, Y)
 
 Y_pred = logisticRegression.predict(X_test[0:10])
 print(data_v2[0:10]) '#Prints the original data; the ground truth'
-print (Y_pred)
+print(Y_pred)
 '#Prints what the income the model predicted, either >50K or <= 50K'
 
 '#Total Accuracy: Correct predictions / Total number of data points'
 total_model_accuracy = logisticRegression.score(X_test, y_test)
 
-print (total_model_accuracy)  '#Model  accuracy is 83.3%'
+print(total_model_accuracy)  '#Model  accuracy is 83.3%'
 """#Additional Resources:
 https://scikit-learn.org/stable/modules/generated/sklearn.
 linear_model.LogisticRegression.html"""
