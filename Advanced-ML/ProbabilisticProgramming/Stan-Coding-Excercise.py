9d4f5adc4fa77ea473f38c33732e47f7ab78a96c
yashn35 <yasnara04@gmail.com>
2019-05-03 14:36:31 -0700

    Aritcle added

--- Advanced-ML/ProbabilisticProgramming/Stan-Coding-Excercise.py
+++ Advanced-ML/ProbabilisticProgramming/Stan-Coding-Excercise.py
@@ -1,3 +1,4 @@
 # https://mc-stan.org/users/documentation/case-studies.html 
 # Stan case studies 
 
+# Article that I read through: https://mc-stan.org/users/documentation/case-studies/pystan_workflow.html
