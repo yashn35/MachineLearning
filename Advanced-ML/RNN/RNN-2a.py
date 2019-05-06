0a9fa13e86fdaf970231cbe920b7e7d7d99dbb19
yashn35 <yasnara04@gmail.com>
2019-05-06 14:22:27 -0700

    Comment changes

--- Advanced-ML/RNN/RNN-2a.py
+++ Advanced-ML/RNN/RNN-2a.py
@@ -10,11 +10,10 @@ from keras.layers import LSTM
 
 two_unit_SRNN = Sequential()
 two_unit_SRNN.add(SimpleRNN(units=2, input_shape=(None, 1), activation='linear', use_bias=False)) 
-"""input shape(None, 1) specifies that there will be one input node for n number of timesteps 
-    which in this case is four below.
-
 """
-
+input shape(None, 1) specifies that there will be one input node for n number of timesteps 
+    which in this case is four below.
+"""
 #Units is equal to 2 nodes in the nueral network 
 
 two_unit_SRNN_weights = two_unit_SRNN.get_weights()
@@ -35,10 +34,3 @@ two_unit_SRNN.get_weights()
 
 # This passes in a single sample with four time steps.
 two_unit_SRNN.predict(numpy.array([ [[3], [3], [7], [5]] ]))
-
-"""
-I was getting this error:
-
-2019-02-21 14:21:53.347427: I tensorflow/core/platform/cpu_feature_guard.cc:141] Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.1 SSE4.2 AVX AVX2 FMA
-2019-02-21 14:21:53.350009: I tensorflow/core/common_runtime/process_util.cc:69] Creating new thread pool with default inter op setting: 8. Tune using inter_op_parallelism_threads for best performance.
-"""
